2a2a2164bad9df730e4ca47a28e8960faf070d65
Mike Mitchell <alchemistikk@gmail.com>
2021-11-05 09:59:32 -0400

    Build csv parser

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,10 @@
 puts 'Event Manager Iniitalized'
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line, index|
+  next if index.zero?
+
+  columns = line.split(',')
+  name = columns[2]
+  puts name
+end
